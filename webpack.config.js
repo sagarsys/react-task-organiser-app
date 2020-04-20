e78c979bd74d429a65ccafe319cf406f17a12bab
sagar <sagar_sys@live.com>
2020-04-20 06:05:57 +0400

    -- basic webpack scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const { getThemeVariables } = require('antd/dist/theme');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'app'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader', // compiles Less to CSS
+          options: {
+            modifyVars: getThemeVariables({
+              dark: true, // enable dark mode
+              compact: true, // enable compact mode
+            }),
+            javascriptEnabled: true,
+          },
+        }],
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles SCSS to CSS
+        }, {
+          loader: 'postcss-loader'
+        }],
+      }
+    ]
+  }
+}
