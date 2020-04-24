2ee8cb15bd0aec52bc8337677b0b9a426a0c4279
sagar <sagar_sys@live.com>
2020-04-24 04:40:14 +0400

    -- add new tasks using redux sagas + redux logger middleware

--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { requestTaskCreation } from '../store/mutations'
 
-export const TaskList = ({ tasks, name }) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
     <div>
       <h3>{name}</h3>
       <div>
         {tasks.map(task => (<div key={task.id}>{task.name}</div>))}
       </div>
+      <button onClick={() => createNewTask(id)}>New task</button>
     </div>
 );
 
@@ -19,4 +21,13 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask: (id) => {
+      console.log('Creating new task', id)
+      dispatch(requestTaskCreation(id))
+    }
+  }
+}
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList)
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,44 @@
-import { createStore } from 'redux';
-import { defaultState } from '../../server/default-state';
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { defaultState } from '../../server/default-state'
+import { createLogger } from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import * as sagas from './sagas.mock'
+import * as mutations from './mutations'
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
-    function reducer(state = defaultState, action) {
-      return state;
-    }
+    combineReducers({
+      tasks(tasks = defaultState.tasks, action) {
+        switch ( action.type ) {
+          case mutations.CREATE_TASK:
+            const { taskId, groupId, ownerId } = action
+            return [
+              ...tasks,
+              {
+                id: taskId,
+                name: 'New task',
+                group: groupId,
+                owner: ownerId,
+                isComplete: false
+              }
+            ]
+        }
+        return tasks
+      },
+      comments(comments = defaultState.comments, action) {
+        return comments
+      },
+      groups(groups = defaultState.groups, action) {
+        return groups
+      },
+      users(users = defaultState.users, action) {
+        return users
+      },
+    }),
+    applyMiddleware(createLogger(), sagaMiddleware)
 )
+
+for ( let saga in sagas ) {
+  sagaMiddleware.run(sagas[saga])
+}
