d7953b304250996e83675376c7df92e90d021a66
sagar <sagar_sys@live.com>
2020-04-24 11:07:44 +0400

    -- add some test for task controller

---
+++ src/server/controllers/task.controller.spec.js
@@ -0,0 +1,25 @@
+import { TaskController } from './task.controller'
+
+(async () => {
+  const now = Date.now()
+  const taskId = `T${now}`
+  console.info(`Running Tests ${now}`)
+  await TaskController.addNewTask({
+    name: `Task name ${now}`,
+    id : taskId,
+    group : 'G1',
+    owner : 'U1',
+    isComplete : false
+  })
+  const task = await TaskController.findTask(taskId)
+  console.log('>>> Created Task:', task)
+
+  const update = {
+    id: taskId,
+    name: `UPDATED Task Name ${Date.now()}`,
+    isComplete: true
+  }
+  await TaskController.updateTask(task.id, update)
+  const updatedTask = await TaskController.findTask(taskId)
+  console.log('>>> Updated Task', updatedTask)
+})()
