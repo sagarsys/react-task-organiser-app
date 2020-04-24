eb80cac264ab878396dfe9333e46cd50568a8724
sagar <sagar_sys@live.com>
2020-04-24 23:36:45 +0400

    -- add some validation in endpoints

--- src/server/controllers/task.controller.js
+++ src/server/controllers/task.controller.js
@@ -4,36 +4,39 @@ import { dbConnect } from '../db/db-connect'
 const taskRouter = express.Router()
 
 taskRouter.get('/:id', async(req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
+  if (!id) res.status(400).send()
   try {
     const task = await TaskController.findTask(id)
     res.status(200).send(task)
   } catch ( e ) {
     console.error(e)
-    res.status(400).send()
+    res.status(500).send(e)
   }
 })
 
 taskRouter.post('/', async(req, res) => {
   const { task } = req.body;
+  if (!task) res.status(400).send()
   try {
     await TaskController.addNewTask(task)
     res.status(200).send()
   } catch ( e ) {
     console.error(e)
-    res.status(400).send()
+    res.status(500).send(e)
   }
 })
 
 taskRouter.patch('/:id', async(req, res) => {
   const { id } = req.params;
   const { task } = req.body;
+  if (!task) res.status(400).send()
   try {
     await TaskController.updateTask(id, task)
     res.status(200).send()
   } catch ( e ) {
     console.error(e)
-    res.status(400).send()
+    res.status(500).send(e)
   }
 })
 
